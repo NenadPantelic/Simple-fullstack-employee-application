3b1df2cb44861341963cfa42d298df80d78441cf
Nenad Pantelic <nenadpantelickg@gmail.com>
2019-06-26 17:36:40 +0200

    Add department insertion on client side

--- employee-app/src/App.js
+++ employee-app/src/App.js
@@ -4,14 +4,28 @@ import './App.css';
 import {Home} from './components/Home';
 import {Department} from './components/Department';
 import {Employee} from './components/Employee';
+import {Navigation} from './components/Navigation'
 
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 function App() {
   return (
+    <BrowserRouter>
     <div className="container">
-      <Home/>
-      <Department/>
-      <Employee/>
+      <h3 className="m-3 d-flex justify-content-center">
+        This is React and Web API demo app.
+      </h3>
+      <h5 className="m-3 d-flex justify-content-center">
+        Welcome to the Employee management portal.
+      </h5>
+      <Navigation/>
+      <Switch>
+        <Route path ='/' component={Home} exact />
+        <Route path ='/department' component={Department} />
+        <Route path ='/employee' component={Employee} /> 
+      </Switch>
+
     </div>
+    </BrowserRouter>
   );
 }
 
--- employee-app/src/components/Department.js
+++ employee-app/src/components/Department.js
@@ -1,18 +1,72 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import { Table } from 'react-bootstrap';
+import {ButtonToolbar, Button} from 'react-bootstrap';
+import { AddDepartment } from './AddDepartment';
+ 
+export class Department extends Component {
 
-export class Department extends Component{
-
-    render(){
-        return (
-
-            <div className="mt-5 d-flex justify-content-left">
-                <h3>
-                    Welcome to the Employee management portal. This is the Department page.
-                </h3>
-            </div>
-
-        )
+    constructor(props) {
+        super(props);
+        this.state = { deps: [], modalShow: false };
     }
 
+    refreshList() {
+        fetch("http://localhost:51316/api/Department")
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ deps: data });
+            });
+
+    }
+
+    componentDidMount() {
+        this.refreshList();
+    }
+
+    componentDidUpdate(){
+        this.refreshList();
+    }
+    render() {
+        const { deps } = this.state;
+        let modalClose = () => this.setState({ modalShow: false });
+        return (
+            <div>
+                <Table className="mt-4" striped bordered hover size="sm">
+                    <thead>
+                        <tr>
+                            <th> DepartmentID</th>
+                            <th> DepartmentName</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {deps.map(dep =>
+                            <tr key={dep.DepartmentID}>
+                                <td> {dep.DepartmentID}</td>
+                                <td> {dep.DepartmentName}</td>
+
+
+                            </tr>)}
+                    </tbody>
+
+                </Table>
+
+
+                <ButtonToolbar>
+                    <Button
+                        variant="primary"
+                        onClick={() => this.setState({ modalShow: true })}
+                    >
+                        Add department
+                    </Button>
+
+                    <AddDepartment
+                        show={this.state.modalShow}
+                        onHide={modalClose}
+                    />
+                </ButtonToolbar>
+            </div>
+
+        );
+    }
 
 }
